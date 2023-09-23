becdca2729c794b7a60d4b608ba19fc20a2570b1
Fernando Barbosa <ofernandobarbosa@gmail.com>
2023-09-23 16:44:06 -0300

    first commit

---
+++ src/main/java/br/edu/ifrs/minicurso/springsolidapi/SpringSolidApiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifrs.minicurso.springsolidapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSolidApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSolidApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifrs/minicurso/springsolidapi/SpringSolidApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifrs.minicurso.springsolidapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSolidApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
